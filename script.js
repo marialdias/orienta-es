6c25762ecdb030fd0aac24f3aa3bb70c4cea57a8
marialdias <diasmalu35@gmail.com>
2025-04-10 22:13:02 -0300

    maria

---
+++ script.js
@@ -0,0 +1,25 @@
+const entrada = document.getElementById('entrada');
+const botao = document.getElementById('add');
+const lista = document.getElementById('lista');
+
+const itens = [];
+
+botao.addEventListener('click',
+    function(){
+        let valor = entrada.value;
+        itens.push(valor);
+        entrada.value = "";
+        console.log(itens)
+        imprimir();
+    }
+
+);
+function imprimir(){
+    lista.innerHTML = "";
+    for(let i = 0;i < itens.length i++){
+        
+    }
+     let li = document.createElement("li")
+     li.textContent = 'Algo'
+     lista.appendChild(li)
+}
